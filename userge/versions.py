eaf66d05dbcfa33b48ecd8ad880b7c7f42afd6ca
Madhavreddy672 <71065430+Madhavreddy672@users.noreply.github.com>
2020-12-04 21:02:29 +0530

    Update

--- userge/versions.py
+++ userge/versions.py
@@ -9,4 +9,4 @@ __micro__ = 1
 
 __python_version__ = f"{version_info[0]}.{version_info[1]}.{version_info[2]}"
 __license__ = "[THUNDERS v3.0](https://github.com/Madhavreddy672/teamthundersuserbot/blob/main/LICENSE)"
-__copyright__ = "[ThunderTeam](https://github.com/itzr00tbyte)"
+__copyright__ = "[ThunderTeam Official](https://t.me/thunder_mirror_Group)"
